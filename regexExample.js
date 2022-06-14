6583184c73a3cdd08953833cefa73d32d6c34423
armod <armods@gmail.com>
2022-06-14 07:45:56 +0200

    wyswietla wszystkie litery ze stinga lub z zakresu

--- regexExample.js
+++ regexExample.js
@@ -3,7 +3,13 @@ let alphabetRegex = /[abcdefghijklmnopqrstuvwxyz]/gi; // Change this line
 let result = quoteSample.match(alphabetRegex); // Change this line
 
 //wypisuje wszystkie litery ze stringa
-console.log(result);
+console.log("1:",result);
 
 //wypisuje konkretny element ze stinga
 console.log(result[10]);
+
+let quoteSample2 = "Blueberry 3.141592653s are delicious.";
+let myRegex = /[h-s2-6]/gi; // Change this line
+let result2 = quoteSample2.match(myRegex); // Change this line
+
+console.log("2:",result);
