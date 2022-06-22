8ee22cb3a06bb30456f2cc116a04a0b30e9af137
armod <armods@gmail.com>
2022-06-22 10:16:42 +0200

    wstawianie tabli do tablicy od zadanego indeksu

--- wstawianie_tablicy_do_tablicy.js
+++ wstawianie_tablicy_do_tablicy.js
@@ -28,3 +28,25 @@ months.splice(4, 1, 'May');
 // replaces 1 element at index 4
 console.log(months);
 // expected output: Array ["Jan", "Feb", "March", "April", "May"] */
+
+//inne rozwiązania
+/* function frankenSplice(arr1, arr2, n) {
+    // It's alive. It's alive!
+    let localArray = arr2.slice();
+    for (let i = 0; i < arr1.length; i++) {
+      localArray.splice(n, 0, arr1[i]);
+      n++;
+    }
+    return localArray;
+  }
+
+  function frankenSplice(arr1, arr2, n) {
+    // It's alive. It's alive!
+    let localArr = arr2.slice();
+    localArr.splice(n, 0, ...arr1);
+    return localArr;
+  }
+
+  function frankenSplice(arr1, arr2, n) {
+    return [...arr2.slice(0, n), ...arr1, ...arr2.slice(n)];
+  } */
