59196536a9fbc06a6cabc5fbc14ee136a8b2715d
armod <armods@gmail.com>
2022-06-23 15:33:22 +0200

    sortowanie tablicy

---
+++ sortowanie.js
@@ -0,0 +1,29 @@
+function getIndexToIns(arr, num) 
+{
+  arr.sort(function(a , b) {return a - b});
+  console.log(arr);
+  console.log("indeks=",arr.indexOf(num));
+  for(let i=0; i<arr.lenght; i++)
+  {
+      console.log(" = ", num, i, arr[i]);
+    if (num < arr[i])
+    {
+      console.log(" = ", num, i, arr[i]);
+      return i;
+    }
+    else return 0;
+  }
+}
+
+getIndexToIns([40, 60], 50);
+getIndexToIns([4, 2, 5, 1, 3], 5);
+getIndexToIns([10, 20, 30, 40, 50], 35);
+
+/* 
+const arr = [1, 42, 5, 21, 3, 9];
+ 
+arr.sort((a, b) => {
+  return a - b;
+});
+
+console.log(arr); */
