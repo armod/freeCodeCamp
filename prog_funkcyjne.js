3008d64d83beadd4bb0c1462bb034d5379346331
armod <armods@gmail.com>
2022-06-27 21:57:30 +0200

    programowannie funkcyjne

--- prog_funkcyjne.js
+++ prog_funkcyjne.js
@@ -28,3 +28,32 @@ console.log(
   tea4GreenTeamFCC,
   tea4BlackTeamFCC
 );
+
+//FUNKCJE NIE POWINNY ZAWIERAC/ZMIENIAC ZMIENNYCH GLOBALNYCH - FUNKCJA POWINNA BYĆ UNIWERSALNA/NEUTRALNA
+// The global variable
+const bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
+
+// Change code below this line
+function add(ar, bookName) {
+  let arr = [...ar];
+  arr.push(bookName);
+  console.log("=",arr);
+  return arr;
+  
+  // Change code above this line
+}
+
+// Change code below this line
+function remove(ar, bookName) 
+{
+  const arr = [...ar];
+  const book_index = arr.indexOf(bookName);
+  if (book_index >= 0) 
+  {
+
+    arr.splice(book_index, 1);
+    return arr;
+
+    // Change code above this line
+    }
+}
