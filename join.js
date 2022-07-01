d4a3254fb003014251ef7160d0a44b294f57b8bf
armod <armods@gmail.com>
2022-07-01 21:38:14 +0200

    funkcja join()

--- join.js
+++ join.js
@@ -11,4 +11,4 @@ function sentensify(str) {
     // Only change code above this line
   }
   console.log(sentensify("May-the-force-be-with-you"));
-  sentensify("May-the-force-be-with-you");
+  sentensify("May-the-force-be-with-you"); //output: May the force be with you
