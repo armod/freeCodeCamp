0a257ad145106f6b6c639b9fdbdcb9d8cdec4e7a
armod <armods@gmail.com>
2022-07-03 00:51:11 +0200

    porównywanie dwoch tablic i szukanie w nich duplikatów

--- duplikaty.js
+++ duplikaty.js
@@ -53,3 +53,27 @@ function diffArray(arr1, arr2)
 
 diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
 diffArray([1, "calf", 3, "piglet"], [1, "calf", 3, 4]);
+
+
+//Inne rozwiązania
+/* function diffArray(arr1, arr2) {
+    return arr1
+      .concat(arr2)
+      .filter(item => !arr1.includes(item) || !arr2.includes(item));
+  }
+  
+  diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]); 
+
+
+
+  //inne
+function diffArray(arr1, arr2) {
+  return [...diff(arr1, arr2), ...diff(arr2, arr1)];
+
+  function diff(a, b) {
+    return a.filter(item => b.indexOf(item) === -1);
+  }
+}
+  
+  */
+
