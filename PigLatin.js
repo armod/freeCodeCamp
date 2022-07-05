c8f53ddfabd74414beb83c6a43b3218229767d4f
armod <armods@gmail.com>
2022-07-05 20:17:08 +0200

    pig latin

--- PigLatin.js
+++ PigLatin.js
@@ -52,3 +52,40 @@ translatePigLatin("glove");
 translatePigLatin("paragraphs");
 translatePigLatin("algorithm"); */
 translatePigLatin("rhythm");
+
+
+//inne rozwiązania
+/* function translatePigLatin(str) {
+    let consonantRegex = /^[^aeiou]+/;
+    let myConsonants = str.match(consonantRegex);
+    return myConsonants !== null
+      ? str
+          .replace(consonantRegex, "")
+          .concat(myConsonants)
+          .concat("ay")
+      : str.concat("way");
+  }
+  
+  translatePigLatin("consonant");
+
+
+  function translatePigLatin(str) {
+    if (str.match(/^[aeiou]/)) return str + "way";
+  
+    const consonantCluster = str.match(/^[^aeiou]+/)[0];
+    return str.substring(consonantCluster.length) + consonantCluster + "ay";
+  }
+  
+  // test here
+  translatePigLatin("consonant");
+
+
+ */
+//   function translatePigLatin(str) {
+//     return str
+//       .replace(/^[aeiou]\w*/, "$&way")
+//       .replace(/(^[^aeiou]+)(\w*)/, "$2$1ay");
+//   }
+  
+//   // test here
+//   translatePigLatin("consonant");
