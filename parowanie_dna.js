5dec7047c2535423cfcdc0bb45ddf2f230191f72
armod <armods@gmail.com>
2022-07-06 21:46:13 +0200

    parowanie znaków

--- parowanie_dna.js
+++ parowanie_dna.js
@@ -25,3 +25,22 @@ function pairElement(str)
 }
 
 pairElement("GCG");
+
+
+//Inne rozwiązanie
+// function pairElement(str) {
+//     //create object for pair lookup
+//     var pairs = {
+//       A: "T",
+//       T: "A",
+//       C: "G",
+//       G: "C"
+//     };
+//     //split string into array of characters
+//     var arr = str.split("");
+//     //map character to array of character and matching pair
+//     return arr.map(x => [x, pairs[x]]);
+//   }
+  
+//   //test here
+//   pairElement("GCG");
