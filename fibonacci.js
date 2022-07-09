3f1076d821aa0598ab50a351e5a7d479c907a272
armod <armods@gmail.com>
2022-07-09 20:16:27 +0200

    fibonacci - dodatkowo sumuje nieparzyste dla <= num

--- fibonacci.js
+++ fibonacci.js
@@ -1,12 +1,35 @@
-function fib(num)
-  {
-    if (num < 2)
+function sumFibs(n) 
+{
+  var sum = 0;
+    let a = 0, b = 1;
+    for(let i=0; i<n; i++)
     {
-      return 1;
+      if(b % 2 != 0 & b <= n)
+      {
+        sum += b;
+        //console.log("niepa", "sum=", sum);
+      }
+      //console.log(b, sum);
+      b += a;
+      a = b - a;
     }
-    else
-    {
-      return (fib(num-2) + fib(num-1));
-    }
-  }
-  console.log(fib(66));
+  console.log(sum);
+  return sum;
+}
+
+sumFibs(4000000);
+
+
+//Rekurencyjnie
+// function fib(num)
+//   {
+//     if (num < 2)
+//     {
+//       return 1;
+//     }
+//     else
+//     {
+//       return (fib(num-2) + fib(num-1));
+//     }
+//   }
+//   console.log(fib(66));
