62f28683a21f14627c2772d7ba54b66ba41ba2b3
armod <armods@gmail.com>
2022-07-19 15:38:41 +0200

    szyfr cezara

--- szyfr_cezara.js
+++ szyfr_cezara.js
@@ -2,9 +2,7 @@ function rot13(str)
 {
   let abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   //console.log(str.charCodeAt(0), str.charAt(0));
-  let num = 65;
-  //console.log(String.fromCharCode(65));
-
+  let tab = [];
   console.log("asd",str.charCodeAt(0), str[0].match(/[A-Z]/g));
   
 
@@ -15,22 +13,28 @@ function rot13(str)
     if(str[i] != str[i].match(/[A-Z]/g))
     {
       console.log(str[i]);
+      tab.push(str[i]);
     }
     else if((str.charCodeAt(i)+13) > 90 & str[i] == str[i].match(/[A-Z]/g))
     {
       //console.log("większy od 90 ->", (str.charCodeAt(i+13)) );
       console.log(str[i], abc[(str.charCodeAt(i)+13) - 91], ">90");
+      tab.push(abc[(str.charCodeAt(i)+13) - 91]);
     }
     else if((str.charCodeAt(i)+13) <= 90  & str[i] == str[i].match(/[A-Z]/g))
     {
       console.log(str[i], abc[(str.charCodeAt(i)+13) - 65], "<90");
+      tab.push(abc[(str.charCodeAt(i)+13) - 65]);
     }
     /* else if(str[i] != str[i].match(/[A-Z]/g) )
     {
       console.log("asd", str[i], (str.charCodeAt(i)))
     } */
   }
-  return str;
+  console.log(tab);
+  console.log(tab.join(""));
+  return tab.join("");
 }
 
 rot13("SERR PBQR PNZC");
+rot13("SERR CVMMN!");
