b2146220b9f2b7eb73eb41335279d58f3a493eba
armod <armods@gmail.com>
2022-07-20 13:20:04 +0200

    telephone number validator

---
+++ telephone_number_validator.js
@@ -0,0 +1,9 @@
+function telephoneCheck(str) 
+{
+  return true;
+}
+
+telephoneCheck("555-555-5555");
+telephoneCheck("1 555)555-5555");
+telephoneCheck("(6054756961)");
+telephoneCheck("10 (757) 622-7382");
