0ffd72b0782eb7c9a89366da84c3d937da199565
armod <armods@gmail.com>
2022-07-20 13:43:54 +0200

    telephone number validator

--- telephone_number_validator.js
+++ telephone_number_validator.js
@@ -1,9 +1,24 @@
 function telephoneCheck(str) 
 {
-  return true;
+  //tworzymy tablice samych cyfr
+  const tab = str.split("").filter(num => num >= 0 & num != " ");
+  if(tab.length === 10 | tab.length === 11 )
+  {
+    console.log("Numer",str,"ma", tab.length, "cyfr.")
+    return true;
+  }
+  else if(tab.length < 10 | tab.length > 11 )
+  {
+    console.log("Numer",str,"ma", tab.length, "cyfr. BŁĄD")
+    return false;
+  }
+  const number = str.match(/^[0-9]/);
+  //console.log(str, number);
+  //console.log(tab, tab.length);
 }
 
 telephoneCheck("555-555-5555");
 telephoneCheck("1 555)555-5555");
 telephoneCheck("(6054756961)");
 telephoneCheck("10 (757) 622-7382");
+telephoneCheck("555-5555");
