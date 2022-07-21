069712cf0a6e13c4dde3084ebb30a04cf7e60513
armod <armods@gmail.com>
2022-07-21 13:38:28 +0200

    telephone number validator

--- telephone_number_validator.js
+++ telephone_number_validator.js
@@ -73,3 +73,14 @@ telephoneCheck("1 555)555-5555");
 telephoneCheck("(6054756961)");
 telephoneCheck("10 (757) 622-7382");
 telephoneCheck("555-5555");
+
+//inne 
+/*  let rex1 = /^(1\s?)?\d{3}([-\s]?)\d{3}\2\d{4}$/,
+        rex2 = /^(1\s?)?\(\d{3}\)\s?\d{3}[-\s]?\d{4}$/;
+
+    if (rex1.test(str)) {
+        return true;
+    }
+    else {
+        return rex2.test(str) ? true : false
+    } */
